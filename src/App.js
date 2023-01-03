bf3e41d29a972e2aa0e9045663dde5285efb5ebd
Vincenz Herzog <herzogv88@gmail.com>
2023-01-03 17:42:27 +0100

    Add App js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import "./App.css";
+//Styles
+import "./styles/app.scss";
+//Components
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackStates from "./components/FeedbackStates";
+import FeedbackForm from "./components/FeedbackForm";
+import AboutLink from "./components/AboutLink";
+import { FeedbackProvider } from "./context/FeedbackContext";
+//Pages
+import About from "./views/About";
 
 function App() {
   return (
-    <div className="App">
-      <div></div>
-    </div>
+    <FeedbackProvider>
+      <Router>
+        <Header></Header>
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <FeedbackForm />
+                  <FeedbackStates />
+                  <FeedbackList />
+                </>
+              }
+            ></Route>
+          </Routes>
+          <Routes>
+            <Route path={"/about/"} element={<About />} />
+          </Routes>
+          <AboutLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
   );
 }
 
